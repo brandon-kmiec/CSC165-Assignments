a27994edaa85a09a8c8eb2e60c4c98bc90269587
Brandon Kmiec <114979976+brandon-kmiec@users.noreply.github.com>
2024-03-09 21:53:17 -0800

    Assignment 1

---
+++ Assignment 1/01a-HelloDolphin/run.bat
@@ -0,0 +1,1 @@
+java --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED -Dsun.java2d.d3d=false -Dsun.java2d.uiScale=1 a1.MyGame
